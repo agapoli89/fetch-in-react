ec4bbc4a2af82a80523e7ab92cd3ce6dee48710f
Agnieszka Polińska <agapoli@gmail.com>
2021-05-24 17:09:33 +0200

    Modify li with props

--- src/components/Word.js
+++ src/components/Word.js
@@ -2,7 +2,7 @@ import './Word.css';
 
 const Word = props => {
     return ( 
-        <li className="li-word">Słowo</li>
+        <li className="li-word">Słowo po angielsku: <strong>{props.english}</strong>.Tłumaczenie: <strong>{props.polish}</strong></li>
     );
 }
  
