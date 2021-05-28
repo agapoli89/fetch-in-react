50dafbe7d6a13b7f925fdf0f53e5eb448ee129d3
Agnieszka Polińska <agapoli@gmail.com>
2021-05-28 16:22:03 +0200

    Remove example with fake database

--- src/components/Word.js
+++
@@ -1,9 +0,0 @@
-import './Word.css';
-
-const Word = props => {
-    return ( 
-        <li className="li-word">Słowo po angielsku: <strong>{props.english}</strong>.Tłumaczenie: <strong>{props.polish}</strong></li>
-    );
-}
- 
-export default Word;
